791557701d895abd3e436c587341c100646cf2e0
alex <alexxgurin@mail.ru>
2016-10-28 19:37:14 +0300

    vse)

--- ConsoleApplication1/ConsoleApplication1/air.h
+++ ConsoleApplication1/ConsoleApplication1/air.h
@@ -1,14 +1,24 @@
 #pragma once 
 class aeroflot
-{  
+{
  char destination[10]; 
- char day[8];
- int time;
+ int day;
+ char time;
  int number;
  char type[8];
 public:  
+	aeroflot (  const aeroflot &A )
+		{
+	
+			day=A.day;
+	};
+	int get_number();
+	char get_dest();
  aeroflot(void);  
+ aeroflot( int a, int b);
+ void print();
  void setflight();
  void getflight();  
+
  ~aeroflot(void); 
 };  
--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -4,39 +4,99 @@
 Создать массив объектов. Вывести:
 a)  список рейсов для заданного пункта назначения;
  b)  список рейсов для заданного дня недели; */
+#include <cstdlib> 
+#include <string>
+#include <cstring>
 #include "air.h"
 #include <stdio.h>
 #include <locale>
+#include <cstring>
 using namespace std;
-
-aeroflot::aeroflot(void) 
+int a;
+char m;
+char ss[50];
+aeroflot::aeroflot(void) //конструктор рейса
 { 
 	cout<<" ~~~~~~~~~~ Конструктор Рейса ~~~~~~~~~~~"<<endl;
 } 
 void aeroflot::setflight()
 {
 	
-
+	cout<<"Введите пункт назначения: "<<endl;
+	cin>>destination;
+	cout<<"Номер рейса: "<<endl;
+	cin>>number;
+	cout<<"Тип самолета:"<<endl;
+	cin>>type;
+	cout<<"Введите день вылета:"<<endl;
+	cin>>day;
+	cout<<"Время вылета:"<<endl;
+	cin>>time;
 } ;
+int aeroflot::get_number()
+{
+return number;}
 void aeroflot::getflight()
 {  
-	
+	cout<<"Пункт назначения: "<<this->destination<<endl;
+	cout<<"Номер рейса: "<<this->number<<endl;
+	cout<<"Тип самолета:"<<this->type<<endl;
+	cout<<"День вылета:"<<this->day<<endl;
+	cout<<"Время вылета:"<<this->time<<endl;
 }  
+void aeroflot::print()
+{  
+	cout<<"Номер рейса: "<<this->number<<endl;
+	cout<<"День вылета:"<<this->day<<endl;
+}  
+/*char aeroflot::get_dest()
+{
+	return destination;
+};*/
  aeroflot::~aeroflot(void)
 { 
-	
-
-	cout<<"####### Деструктор Рейса #######"<<endl;
-	
+	cout<<"####### Деструктор Рейса #######"<<endl;	
 }
+ aeroflot::aeroflot(int a,int b)
+ {cout<<"Явный конструктор"<<endl;
+ 
+	 day=a;
+ number=b;
+ };
 int main ()
-{setlocale(LC_ALL,"rus");
-
-
-aeroflot *x; 
-for (int i=0;i<5;i++)
 {
-	x = new aeroflot[i];
+	setlocale(LC_ALL,"rus");
+		aeroflot as(11,22);
+		as.print();
+aeroflot *x=new aeroflot[4]; 
+for (int i=1;i<5;i++)
+{
+	cout<<"Рейс №:"<<i<<endl;
+	x[i].setflight();
 };
+for (int j=1;j<5;j++)
+{
+cout<<"Рейс №:"<<j<<endl;
+	x[j].getflight();
+};
+cout<<"Поиск номеру рейса:"<<endl;
+cin>>a;
+for (int i=1;i<5;i++)
+{
+	if (x[i].get_number() ==a)
+		x[i].getflight();
 
-};
+}
+
+cout<<"Поиск по пункту прибытия"<<endl;
+cin>>m;
+for (int i=1;i<5;i++)
+{
+	if (x[i].get_dest()==m)
+		x[i].getflight();
+
+}
+system("pause");
+delete [] x;
+
+}
